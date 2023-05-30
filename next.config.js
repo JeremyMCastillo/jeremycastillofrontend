b8e4bea8adb4406f9dcc8b4a06f54c7e7666e837
Jeremy Castillo <jcast029.jc@gmail.com>
2023-05-29 22:10:55 -0700

    Initial Commit
    Signed-off-by: Jeremy Castillo <jcast029.jc@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        loader: "default",
+        domains: ["localhost", "127.0.0.1"]
+    }
+}
 
 module.exports = nextConfig
